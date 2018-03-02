63b4f5975b83ca90d985205cff4d348ec118d901
Rails Udemy Course <opacic.milos_95@hotmail.com>
2018-03-02 17:37:48 +0100

    Add search stock ajax and errors

---
+++ app/models/stock.rb
@@ -0,0 +1,16 @@
+class Stock < ApplicationRecord
+
+	def self.strip_commas(num)
+		num.gsub(",","")
+	end
+
+	def self.new_from_lookup(ticker_symbol)
+		begin
+			looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
+			price = strip_commas(looked_up_stock.l)
+			new(name: looked_up_stock.name, ticker: looked_up_stock.symbol, last_price: price)
+		rescue Exception => e 
+			return nil
+		end
+	end
+end
