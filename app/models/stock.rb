03986be214062565a6d445ec6c9a2515619ee224
Rails Udemy Course <opacic.milos_95@hotmail.com>
2018-03-03 15:32:13 +0100

    Add user and stock association plus views

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,7 +1,14 @@
 class Stock < ApplicationRecord
 
+	has_many :user_stocks
+	has_many :users, through: :user_stocks
+
 	def self.strip_commas(num)
 		num.gsub(",","")
+	end
+
+	def self.find_by_ticker(ticker_symbol)
+		where(ticker: ticker_symbol).first
 	end
 
 	def self.new_from_lookup(ticker_symbol)
